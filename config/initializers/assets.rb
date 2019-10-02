e799acddf98079efc55fd7adca72480b2d10b664
Rigoberto Alcantar Aguilar <a20150338@utem.edu.mx>
2019-10-01 19:02:00 -0500

    NavBar

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,8 +13,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 
-Rails.application.config.assets.precompile += %w( slick.css )
-Rails.application.config.assets.precompile += %w( slick-theme.css )
-Rails.application.config.assets.precompile += %w( nouislider.min.css )
-Rails.application.config.assets.precompile += %w( font-awesome.min.css )
-Rails.application.config.assets.precompile += %w( style.css )
+Rails.application.config.assets.precompile += %w( custom.css )
